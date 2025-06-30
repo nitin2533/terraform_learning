0ee2fe7adebb64294dc6fa81ff684d27bccf0003
nitin2533 <nitin2533@gmail.com>
2025-06-30 15:26:03 +0530

    new code

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.34.0"
+    }
+  }
+}
+provider "azurerm" {
+  features {
+    
+  }
+  subscription_id = "00eee853-16f9-4fd6-bf25-8aba491040dd"
+}
+resource "azurerm_resource_group" "examplerg" {
+  name     = "mmmrg"
+  location = "West Europe"
+}
+resource "azurerm_virtual_network" "examplevnet" {
+  name                = "mmmvnet"
+  location            = azurerm_resource_group.examplerg.location
+  resource_group_name = azurerm_resource_group.examplerg.name
+  address_space       = ["10.0.0.0/16"]
+}
