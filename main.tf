71edf9c0e21a4e560bc1a546b2ab70d741915012
nitin2533 <nitin2533@gmail.com>
2025-06-30 15:42:23 +0530

    newcode

--- main.tf
+++ main.tf
@@ -29,3 +29,41 @@ resource "azurerm_subnet" "examplesubnet1" {
   virtual_network_name = azurerm_virtual_network.examplevnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
+
+resource "azurerm_network_interface" "examplenic" {
+  name                = "mmmnic"
+  location            = azurerm_resource_group.examplerg.location
+  resource_group_name = azurerm_resource_group.examplerg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.examplesubnet1.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "examplevm" {
+  name                = "mmmvm"
+  resource_group_name = azurerm_resource_group.examplerg.name
+  location            = azurerm_resource_group.examplerg.location
+  size                = "Standard_F2"
+  admin_username      = "admin_user"
+  admin_password      = "admin@123"
+  disable_password_authentication = "false"
+  network_interface_ids = [
+    azurerm_network_interface.examplenic.id,
+  ]
+
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
